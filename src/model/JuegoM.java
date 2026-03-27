f10270b913bdd2101da0dbf42eda79374338d2af
EliseoC45 <eliseo45310@gmail.com>
2026-03-27 03:52:58 -0300

    Fix variables and Letras Amarillas de mas

--- src/model/JuegoM.java
+++ src/model/JuegoM.java
@@ -6,11 +6,11 @@ import events.IJuegoObserver;
 
 public class JuegoM extends Observable<IJuegoObserver> {
 	private String palabraSecreta;
-	private int intentos;
-	private String[] diccionario;
+	private int intentosRestantes;
+	private String[] listaDePalabras;
 	
 	public JuegoM(int cantVidas) {
-		this.diccionario = new String[] {
+		this.listaDePalabras = new String[] {
 				"ACTOR", "AGUAS", "ALTOS", "AMIGO", "ARBOL", "AUTOS", "AVION", "BARCO", "BESOS", "BRAZO",
 			    "CAJAS", "CALOR", "CAMAS", "CAMPO", "CANTO", "CARAS", "CARRO", "CASAS", "CERDO", "ENANO",
 			    "CINCO", "CLARO", "CLIMA", "COCHE", "COLOR", "CORTE", "COSAS", "CREMA", "DADOS", "DATOS",
@@ -18,27 +18,39 @@ public class JuegoM extends Observable<IJuegoObserver> {
 			    "HOJAS", "HORAS", "HUESO", "IDEAS", "ISLAS", "JABON", "JOVEN", "JUEGO", "LAPIZ", "LIBRO"
 			    };
 		Random generadorAleatorio = new Random();
-		int indiceAleatorio = generadorAleatorio.nextInt(this.diccionario.length);
-		this.palabraSecreta = this.diccionario[indiceAleatorio];
-		this.intentos = cantVidas;
+		int indiceAleatorio = generadorAleatorio.nextInt(this.listaDePalabras.length);
+		this.palabraSecreta = this.listaDePalabras[indiceAleatorio];
+		this.intentosRestantes = cantVidas;
 	}
 	public void validarPalabra(String palabraIngresada) {
 		String[] coloresLetras = new String[5];
 		palabraIngresada = palabraIngresada.toUpperCase();
-		
+		StringBuilder palabraSecretaTemp = new StringBuilder(this.palabraSecreta);
+		// Primero, marcar las letras correctas 
 		for (int i = 0; i < 5; i++) {
-			if (palabraIngresada.charAt(i) == palabraSecreta.charAt(i)) {
+			if(palabraIngresada.charAt(i) == palabraSecretaTemp.charAt(i)) {
 				coloresLetras[i] = "verde";
-			} else if (palabraSecreta.contains(String.valueOf(palabraIngresada.charAt(i)))) {
-				coloresLetras[i] = "amarillo";
+				palabraSecretaTemp.setCharAt(i, '-');
 			} else {
 				coloresLetras[i] = "gris";
 			}
 		}
-		this.intentos--;
+		
+		// Luego, marcar las letras amarillas
+		for (int i = 0; i < 5; i++) {
+			if (coloresLetras[i].equals("gris")) {
+				int index = palabraSecreta.indexOf(String.valueOf(palabraIngresada.charAt(i)));
+				if (index != -1) {
+					coloresLetras[i] = "amarillo";
+					palabraSecretaTemp.setCharAt(index, '-');
+				}
+			}
+		}
+		
+		this.intentosRestantes--;
 		boolean gano = palabraIngresada.equals(palabraSecreta);
 		notifyObservers(observer -> observer.onPalabraRevisada(coloresLetras));
-		if (gano || intentos == 0) {
+		if (gano || intentosRestantes == 0) {
 			notifyObservers(observer -> observer.onJuegoTerminado(gano));
 		}
 	}
