4a1eebc4e30e0c99710849124dd76cb88a4ef4b6
Nahuel Nievas <nahuelnievas30@gmail.com>
2026-03-28 23:56:54 -0300

    Dialog/Juego
    feat: create Juego controller.
    feat: create dialog.
    feat: create game over view.
    feat: complete process end to end Juego (controller, view, model).

--- src/view/GameOverView.java
+++ src/view/GameOverView.java
@@ -2,6 +2,21 @@ package view;
 
 import javax.swing.JPanel;
 
-public class GameOverView extends JPanel {
+import java.awt.Font;
 
+import javax.swing.JLabel;
+
+public class GameOverView extends JPanel {
+	public GameOverView() {
+		setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 0, 9, 9);
+		add(panel);
+		
+		JLabel lblNewLabel = new JLabel("GAME OVER");
+		lblNewLabel.setFont(new Font("SansSerif", Font.BOLD, 18));
+		lblNewLabel.setBounds(197, 68, 135, 58);
+		add(lblNewLabel);
+	}
 }
