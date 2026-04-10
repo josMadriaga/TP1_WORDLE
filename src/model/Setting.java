67ea9ee90f892d0c998c3d65511adf3c07b6cc9c
Nahuel Nievas <nahuelnievas30@gmail.com>
2026-04-09 21:16:05 -0300

    feat: add prop files with translations
    feat: language util
    
    fix: fixes

--- src/model/Setting.java
+++ src/model/Setting.java
@@ -3,25 +3,38 @@ package model;
 import events.ISettingsObserver;
 import utils.DifficultEnum;
 import utils.LanguageEnum;
+import utils.LanguageUtil;
 import utils.Observable;
 
 public class Setting extends Observable<ISettingsObserver> {
 	private DifficultEnum dificultad;
 	private LanguageEnum lenguaje;
-	
+
 	public Setting() {
 		this.dificultad = DifficultEnum.EASY;
 		this.lenguaje = LanguageEnum.SPANISH;
 	}
-	
+
 	public void updateDifficult(DifficultEnum newDifficult) {
 		this.dificultad = newDifficult;
 		notifyObservers(observer -> observer.onDifficultChanged(newDifficult));
 	}
-	
+
 	public void updateLanguage(LanguageEnum newLanguage) {
 		this.lenguaje = newLanguage;
+		setGlobalLanguage();
 		notifyObservers(observer -> observer.onLanguageChanged(newLanguage));
+	}
+
+	private void setGlobalLanguage() {
+		switch (this.lenguaje) {
+		case LanguageEnum.ENGLISH:
+			LanguageUtil.setIdioma("en");
+			break;
+		case LanguageEnum.SPANISH:
+			LanguageUtil.setIdioma("es");
+			break;
+		}
 	}
 
 	public DifficultEnum getDificultad() {
